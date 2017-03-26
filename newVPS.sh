3248291e28dd7a6c394c51cf4b491622185786e7
chaideblog <chai_jinlong@163.com>
2017-03-26 22:15:23 +0800

    1.0.1
    添加功能：
    * 安装配置aria2
    * 安装配置lighttpd

---
+++ newVPS.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# 初始化
+sudo mkdir -p ~/application
+
+# 安装aria2
+sudo mkdir -p ~/application/aria2
+sudo mkdir -p ~/download
+sudo apt-get install aria2 -y
+cp ~/VPS/aria2/aria2.conf ~/application/aria2/
+sudo aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all -c -D
+aria2c --conf-path=~/application/aria2/aria2.conf --disable-ipv6=true -D
+sudo aria2c --conf-path=~/application/aria2/aria2.conf --disable-ipv6=true -D &
+echo "\n"
+
+# 安装lighttpd
+sudo apt-get install lighttpd -y
+sudo cp ~/VPS/lighttpd/lighttpd.conf /etc/lighttpd/
+sudo service lighttpd restart
