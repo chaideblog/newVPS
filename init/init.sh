ea3ffa9ae199253972dc2474246a4bdf9582b00a
chaideblog <chai_jinlong@163.com>
2017-03-28 09:12:16 +0800

    1.0.8
    添加功能：
    * 自动修改init.sh中变量
    * 可以修改服务的端口和密码等信息

--- init/init.sh
+++ init/init.sh
@@ -1,21 +1,16 @@
-#!/bin/bash
 #-----------------------需要更改部分-----------------------#
-# 用户目录
-HOME_DIR="/home/ubuntu"
-
 # 需要开机自启的软件，true自启
 ARIA2=true
 LIGHTTPD=true
 RESILIO_SYNC=true
 SHADOWSOCKS=true
-
 #-----------------------请勿更改部分-----------------------#
 # 启动aria2
 if "$ARIA2" = true;
 then
 	sudo aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all -c -D
-	aria2c --conf-path=${HOME_DIR}/application/aria2/aria2.conf --disable-ipv6=true -D
-	sudo aria2c --conf-path=${HOME_DIR}/application/aria2/aria2.conf --disable-ipv6=true -D &
+	aria2c --conf-path=${HOME_PATH}/application/aria2/aria2.conf --disable-ipv6=true -D
+	sudo aria2c --conf-path=${HOME_PATH}/application/aria2/aria2.conf --disable-ipv6=true -D &
 	echo "\n"
 fi
 
@@ -28,11 +23,11 @@ fi
 # 启动resilio sync
 if "$RESILIO_SYNC" = true;
 then
-	sudo ${HOME_DIR}/application/resilio_sync/rslsync --webui.listen 0.0.0.0:8888
+	sudo ${HOME_PATH}/application/resilio_sync/rslsync --webui.listen 0.0.0.0:${RESILIO_SYNC_PORT}
 fi
 
 # 安装shadowsocks
 if "$SHADOWSOCKS" = true;
 then
-	sudo nohup ssserver -c /etc/shadowsocks/shadowsocks.json start > shadowsocks.out 2>&1 &
+	sudo nohup ssserver -c /etc/shadowsocks/shadowsocks.json start > ${HOME_PATH}/application/shadowsocks/shadowsocks.out 2>&1 &
 fi
--- setupVPS.sh
+++ setupVPS.sh
@@ -1,12 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 #-----------------------需要更改部分-----------------------#
 # 安装的用户目录
 BOOT_START=true		# 开机是否自启
-HOME_DIR="/home/ubuntu"
-MY_IP="192.168.30.110"
-
-# 需要配置的密码
-SHADOWSOCKS_PASSWD="123456"
+HOME_PATH="/home/ubuntu"		# 安装的用户目录
+MY_IP="192.168.30.110"		# 主机的ip地址
 
 # 需要安装的软件，true安装
 ARIA2=true
@@ -18,22 +15,45 @@ SHADOWSOCKS=true
 GIT=true
 ZIP=true
 ZIPROXY=false
+#-----------------------软件配置部分-----------------------#
+# aira
+ARIA_FILE_SAVE_PATH="${HOME_PATH}/download"
+ARIA_PORT="6800"
+ARIA_USERNAME="tencent"
+ARIA_PASSWD="ariapassword"
+
+# lighttpd
+LIGHTTPD_PORT="8008"
+LIGHTTPD_PATH="${HOME_PATH}/download"
+LIGHTTPD_USERNAME="www-data"
+LIGHTTPD_GROUPNAME="www-data"
+
+# shadowsocks
+SHADOWSOCKS_PASSWD="123456"
+SHADOWSOCKS_PORT="8388"
+
+# resilio sync
+RESILIO_SYNC_PORT="8888"
 #-----------------------请勿更改部分-----------------------#
 # 初始化
 sudo apt update
 sudo apt-get update
-sudo mkdir -p ${HOME_DIR}/application
+sudo mkdir -p ${HOME_PATH}/application
 
 # 安装aria2
 if "$ARIA2" = true;
 then
-	sudo mkdir -p ${HOME_DIR}/application/aria2
-	sudo mkdir -p ${HOME_DIR}/download
+	sudo mkdir -p ${HOME_PATH}/application/aria2
+	sudo mkdir -p ${ARIA_FILE_SAVE_PATH}
 	sudo apt-get install aria2 -y
-	cp ${HOME_DIR}/newVPS/aria2/aria2.conf ${HOME_DIR}/application/aria2/
+	sudo sed -i "s#ARIA_FILE_SAVE_PATH#${ARIA_FILE_SAVE_PATH}#g" ${HOME_PATH}/newVPS/aria2/aria2.conf
+	sudo sed -i "s#ARIA_PORT#${ARIA_PORT}#g" ${HOME_PATH}/newVPS/aria2/aria2.conf
+	sudo sed -i "s#ARIA_USERNAME#${ARIA_USERNAME}#g" ${HOME_PATH}/newVPS/aria2/aria2.conf
+	sudo sed -i "s#ARIA_PASSWD#${ARIA_PASSWD}#g" ${HOME_PATH}/newVPS/aria2/aria2.conf
+	cp ${HOME_PATH}/newVPS/aria2/aria2.conf ${HOME_PATH}/application/aria2/
 	sudo aria2c --enable-rpc --rpc-listen-all=true --rpc-allow-origin-all -c -D
-	aria2c --conf-path=${HOME_DIR}/application/aria2/aria2.conf --disable-ipv6=true -D
-	sudo aria2c --conf-path=${HOME_DIR}/application/aria2/aria2.conf --disable-ipv6=true -D &
+	aria2c --conf-path=${HOME_PATH}/application/aria2/aria2.conf --disable-ipv6=true -D
+	sudo aria2c --conf-path=${HOME_PATH}/application/aria2/aria2.conf --disable-ipv6=true -D &
 	echo "\n"
 fi
 
@@ -41,7 +61,11 @@ fi
 if "$LIGHTTPD" = true;
 then
 	sudo apt-get install lighttpd -y
-	sudo cp ${HOME_DIR}/newVPS/lighttpd/lighttpd.conf /etc/lighttpd/
+	sudo sed -i "s#LIGHTTPD_PORT#${LIGHTTPD_PORT}#g" ${HOME_PATH}/newVPS/lighttpd/lighttpd.conf
+	sudo sed -i "s#LIGHTTPD_PATH#${LIGHTTPD_PATH}#g" ${HOME_PATH}/newVPS/lighttpd/lighttpd.conf
+	sudo sed -i "s#LIGHTTPD_USERNAME#${LIGHTTPD_USERNAME}#g" ${HOME_PATH}/newVPS/lighttpd/lighttpd.conf
+	sudo sed -i "s#LIGHTTPD_GROUPNAME#${LIGHTTPD_GROUPNAME}#g" ${HOME_PATH}/newVPS/lighttpd/lighttpd.conf
+	sudo cp ${HOME_PATH}/newVPS/lighttpd/lighttpd.conf /etc/lighttpd/
 	sudo service lighttpd restart
 fi
 
@@ -60,11 +84,11 @@ fi
 # 安装Resilio Sync
 if "$RESILIO_SYNC" = true;
 then
-	sudo mkdir -p ${HOME_DIR}/application/resilio_sync
-	sudo cp ${HOME_DIR}/newVPS/resilio_sync/resilio-sync_x64.tar ${HOME_DIR}/application/resilio_sync
-	cd ${HOME_DIR}/application/resilio_sync
+	sudo mkdir -p ${HOME_PATH}/application/resilio_sync
+	sudo cp ${HOME_PATH}/newVPS/resilio_sync/resilio-sync_x64.tar ${HOME_PATH}/application/resilio_sync
+	cd ${HOME_PATH}/application/resilio_sync
 	sudo tar xvf resilio-sync_x64.tar
-	sudo ${HOME_DIR}/application/resilio_sync/rslsync --webui.listen 0.0.0.0:8888
+	sudo ${HOME_PATH}/application/resilio_sync/rslsync --webui.listen 0.0.0.0:${RESILIO_SYNC_PORT}
 fi
 
 # 安装shadowsocks
@@ -73,7 +97,7 @@ then
 	sudo apt install shadowsocks -y
 	sudo echo "{" >> /etc/shadowsocks/shadowsocks.json
 	sudo echo "\"server\":\"${MY_IP}\"," >> /etc/shadowsocks/shadowsocks.json
-	sudo echo "\"server_port\":8388," >> /etc/shadowsocks/shadowsocks.json
+	sudo echo "\"server_port\":${SHADOWSOCKS_PORT}," >> /etc/shadowsocks/shadowsocks.json
 	sudo echo "\"local_address\":\"127.0.0.1\"," >> /etc/shadowsocks/shadowsocks.json
 	sudo echo "\"local_port\":1080," >> /etc/shadowsocks/shadowsocks.json
 	sudo echo "\"password\":\"${SHADOWSOCKS_PASSWD}\"," >> /etc/shadowsocks/shadowsocks.json
@@ -82,8 +106,8 @@ then
 	sudo echo "\"fast_open\":false," >> /etc/shadowsocks/shadowsocks.json
 	sudo echo "}" >> /etc/shadowsocks/shadowsocks.json
 
-	sudo mkdir -p ${HOME_DIR}/application/shadowsocks
-	sudo nohup ssserver -c /etc/shadowsocks/shadowsocks.json start > ${HOME_DIR}/application/shadowsocks/shadowsocks.out 2>&1 &
+	sudo mkdir -p ${HOME_PATH}/application/shadowsocks
+	sudo nohup ssserver -c /etc/shadowsocks/shadowsocks.json start > ${HOME_PATH}/application/shadowsocks/shadowsocks.out 2>&1 &
 fi
 
 # 安装git
@@ -107,10 +131,15 @@ fi
 # 配置开启自启
 if "$BOOT_START" = true;
 then
-	sudo mkdir -p ${HOME_DIR}/application/init
-	sudo cp ${HOME_DIR}/newVPS/init/init.sh ${HOME_DIR}/application/init
-	sudo chmod +x ${HOME_DIR}/application/init/init.sh
-	sudo sed -i "s/exit 0/\n/g" /etc/rc.local
-	sudo echo "${HOME_DIR}/application/init/init.sh" >> /etc/rc.local
+	sudo mkdir -p ${HOME_PATH}/application/init
+	echo "#!/bin/bash" >> ${HOME_PATH}/application/init/init.sh
+	echo "# 引用的变量" >> ${HOME_PATH}/application/init/init.sh
+	echo "HOME_PATH=\"${HOME_PATH}\"		# 安装的用户目录" >> ${HOME_PATH}/application/init/init.sh
+	echo "RESILIO_SYNC_PORT=${RESILIO_SYNC_PORT}" >> ${HOME_PATH}/application/init/init.sh
+
+	sudo cat ${HOME_PATH}/newVPS/init/init.sh ${HOME_PATH}/application/init/init.sh
+	sudo chmod +x ${HOME_PATH}/application/init/init.sh
+	sudo sed -i "s#exit 0#\n#g" /etc/rc.local
+	sudo echo "${HOME_PATH}/application/init/init.sh" >> /etc/rc.local
 	sudo echo "exit 0" >> /etc/rc.local
 fi
